a90f6a58991655cdb92f59852ffad384b87a231a
Swapnil <sswapnil_be20@thapar.edu>
2024-04-02 05:41:49 +0530

    added html to render
    Signed-off-by: Swapnil <sswapnil_be20@thapar.edu>

--- api/index.py
+++ api/index.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return jsonify({'status': 'success', 'message': 'server is running 🚀'})
 
+@app.route('/render')
+def about():
+    return render_template('index.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
